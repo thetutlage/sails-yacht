29cc8fc82ac763ea984a426a5f23971752413681
Harminder Virk <virk.officials@gmail.com>
2014-09-26 11:12:07 +0530

    Added support for revisions --beta

--- index.js
+++ index.js
@@ -5,24 +5,31 @@ var validator = yacht.validator;
 var orm = yacht.orm;
 var prettyjson = require('prettyjson');
 
-validator.__globals(true,true,true,true);
+//validator.__globals(true,true,true,true);
 
 errMessage = {
     "username": [
         {
             "value": "Amanvirk",
+            "rule": "required",
+            "message": "A record with that `username` already exists (`996806500`)."
+        },
+        {
+            "value": "",
             "rule": "unique",
             "message": "A record with that `username` already exists (`996806500`)."
         },
         {
             "value": "",
-            "rule": "required",
-            "message": "A record with that `username` already exists (`996806500`)."        	
-        },
+            "rule": "date",
+            "message": "Not a date"
+        }
+    ],
+    "phone_no":[
         {
             "value": "",
-            "rule": "date",
-            "message": "A record with that `username` already exists (`996806500`)."         	
+            "rule": "required",
+            "message": "A record with that phone_no is required"
         }
     ]
 };
@@ -35,15 +42,47 @@ var validationMessages = {
 	  	}
 	}
  };
-
+// validator.__defaults.showFields = true;
+// validator.__defaults.showMessages = true;
+// validator.__defaults.showDescriptive = false;
 // validator.validate(validationMessages,errMessage,function(messages){
-// 	console.log(JSON.stringify(messages));
+//         console.log(prettyjson.render(messages));
 // });
 
 var User = {
     updateFields: ['displayName','isdisplayName','profile_pic','phone_no','sound_cloud_id','sound_cloud_username','sound_cloud_token','sound_cloud_refresh_token','account_status'],
 };
-var f = { username: '996806500', phone_no: '996806500','profile_pic': 'Wow','random':'isThere'};
-orm.setUpdateFields(User,f,function(fields){
-    console.log(fields);
-});
+
+var recordUpdated = { id: 1,
+  username: 'amanvirk',
+  email_address: 'virk.officials@gmail.com',
+  password: null,
+  alternate_email_address: null,
+  contact_no: null,
+  account_status: 'Pending',
+  is_verified: 0,
+  verification_token: null,
+  createdAt: 'Thu Sep 25 2014 18:54:15 GMT+0530 (IST)',
+  updatedAt: 'Fri Sep 26 2014 10:07:52 GMT+0530 (IST)' 
+}
+
+var items = { id: 1,
+  username: 'harminder',
+  email_address: 'virk.officials@gmail.com',
+  password: null,
+  alternate_email_address: null,
+  contact_no: null,
+  account_status: 'Pending',
+  is_verified: 0,
+  verification_token: null,
+  createdAt: 'Thu Sep 25 2014 18:54:15 GMT+0530 (IST)',
+  updatedAt: 'Fri Sep 26 2014 10:07:52 GMT+0530 (IST)' 
+}
+
+orm.initRevision(items);
+orm.createRevision(User,recordUpdated);
+
+// var f = { username: '996806500', phone_no: '996806500','profile_pic': 'Wow','random':'isThere'};
+// orm.setUpdateFields(User,f,function(fields){
+//     console.log(fields);
+// });
--- yacht.js
+++ yacht.js
@@ -1,97 +1,161 @@
+/**
+	Module Dependencies
+	Underscorejs
+*/
 var _ = require("underscore");
 
-var yacht = module.exports;
-var validator = {};
-var orm = {};
-var globals = {};
-globals.showCount = false;
-globals.listFields = false;
-globals.showMessages = false;
-globals.longMessages = false;
+/**
+	* Sails yacht provides an extra layer on top of sailsjs models by adding features like :-
+		
+		*	Custom validation messages
+		*	Update revisions
+		*	Defining update fields
 
-errorsCounts = 0;
-var fields = [];
-var messages = {};
-var descriptive = {};
+	* If you don't need any of these functionalities simply don't call any methods.
 
-function __globals(c,f,m,d){
-	globals.showCount = c;
-	globals.listFields = f;
-	globals.showMessages = m;
-	globals.longMessages = d;
-}
+	------------------------------------------------------------------------------------
+	** HOW IT WORKS **
+	------------------------------------------------------------------------------------
 
-function validate(Model,Err,callback){
+	* Validator
+		
+		*	Validator exposes only single method called validate(model,error,callback) to generate custom validation
+			messages.
 
-	var __messages = Model.validationMessages;
-	var __fields = _.keys(__messages);
-	var i = 0;
+	* ORM
 
-	if(globals.showCount){
-		errorsCounts = _.size(Err);
-	}
-	if(globals.listFields){
-		fields = __fields;
-	}
-	_.each(Err,function(v,k){
-		i++;
-		if(_.contains(__fields,k)){
-			descriptive[k] = [];
-			_.each(Err[k],function(values,keys){
-				if(typeof(__messages[k][values.rule]) !== 'undefined'){
-					if(globals.showMessages){
-						messages[k] = __messages[k][values.rule].replace('%value%',[values.value]);
+		*	Orm exposes couple of methods defined below
+				
+				* setUpdateFields(model,update_values,callback)
+				* setRevisions(model,values,callback)
+
+*/
+
+var validator = {
+
+	__defaults:{
+		showCount: true,
+		showFields: false,
+		showMessages: false,
+		showDescriptive: true
+	},
+
+	__globals:{
+		counts:0,
+		fields:[],
+		messages:{},
+		descriptive:{}
+	},
+
+	__helpers:{
+		messages:[],
+		fields:[]
+	},
+
+	validate: function(Model,Err,Cb){
+		var obj = this;
+		obj.__helpers.messages = Model.validationMessages;
+		obj.__helpers.fields = _.keys(this.__helpers.messages);
+		if(obj.__defaults.showCount){
+			obj.__globals.counts = _.size(Err);
+		}
+		if(obj.__defaults.showFields){
+			obj.__globals.fields = _.keys(Err);
+		}
+		_.each(Err,function(errO,errK){
+
+			if(_.contains(obj.__helpers.fields,errK)){
+				// Error fields are part of custom defined validationMessages.
+				
+				obj.setGlobals(Err,errK,true);
+			}else{
+				// Error fields are not part of custom defined validationMessages. 
+				// Hence original messages will be used.
+				obj.setGlobals(Err,errK,false);
+			}
+		});
+		Cb(obj.__globals);
+	},
+
+	setGlobals: function(Err,item,isDefined,Cb){
+		var obj = this;
+		if(obj.__defaults.showDescriptive){
+			obj.__globals.descriptive[item] = [];
+		}
+		_.each(Err[item],function(values,keys){
+			if(isDefined){
+				if(typeof(obj.__helpers.messages[item][values.rule]) !== 'undefined'){
+					if(obj.__defaults.showMessages){
+						if(typeof(obj.__globals.messages[item]) == 'undefined'){
+							obj.__globals.messages[item] = obj.__helpers.messages[item][values.rule].replace('%value%',[values.value]);
+						}
 					}
-					if(globals.longMessages){
-						descriptive[k].push({value:values.value,rule:values.rule,message:__messages[k][values.rule].replace('%value%',[values.value])});
+					if(obj.__defaults.showDescriptive){
+						obj.__globals.descriptive[item].push({value:values.value,rule:values.rule,message:obj.__helpers.messages[item][values.rule].replace('%value%',[values.value])});
 					}
 				}else{
-					descriptive[k].push(values);
+					if(obj.__defaults.showMessages){
+						if(typeof(obj.__globals.messages[item]) == 'undefined'){
+							obj.__globals.messages[item] = values.message;
+						}
+					}
+					if(obj.__defaults.showDescriptive){
+						obj.__globals.descriptive[item].push(values);
+					}
 				}
-			});
-		}
-		if(i == _.size(Err)){
-			callback(validationMessages());
-		}
-	});
+			}else{
+				if(obj.__defaults.showMessages){
+					if(typeof(obj.__globals.messages[item]) == 'undefined'){
+						obj.__globals.messages[item] = values.message;
+					}
+				}
+				if(obj.__defaults.showDescriptive){
+					obj.__globals.descriptive[item].push(values);
+				}
+			}
+		});
+	}
+};
+
+var orm = {
+
+	__helpers:{
+		fields: [],
+		past: [],
+		present: [],
+		future: []
+	},
+
+	setUpdateFields: function(Model,values,callback){
+		obj.__helpers.fields = Model.updateFields;
+		var size = _.size(values);
+		var i = 0;
+		_.each(values,function(data,keys){
+			i++;
+			if(!_.contains(obj.__helpers.fields,keys)){
+				delete values[keys];
+			}
+			if(i == size){
+				callback(values);
+			}
+		});
+	},
+
+	initRevision: function(Model,values,callback){
+		this.__helpers.past = values;
+	},
+	createRevision: function(Model,values){
+		var obj = this;
+		obj.__helpers.present = values;
+		_.each(obj.__helpers.past,function(v,k){
+			if(obj.__helpers.past[k] !== obj.__helpers.present[k]){
+				obj.__helpers.future[k] = v;
+			}
+		});
+		console.log(obj.__helpers.future);
+	}
 }
 
-function setUpdateFields(Model,values,callback){
-	var __fields = Model.updateFields;
-	var intersect = [];
-	var __size = _.size(values);
-	var i = 0;
-	_.each(values,function(data,keys){
-		i++;
-		if(!_.contains(__fields,keys)){
-			delete values[keys];
-		}
-		if(i == __size){
-			callback(values);
-		}
-	});
-}
-
-function validationMessages(){
-	var rd = {};
-	if(globals.showCount){
-		rd.count = errorsCounts;
-	}
-	if(globals.listFields){
-		rd.fields = fields;
-	}
-	if(globals.showMessages){
-		rd.messages = messages;
-	}
-	if(globals.longMessages){
-		rd.descriptive = descriptive;
-	}
-	return rd;
-}
-
+var yacht = module.exports;
 yacht.validator = validator;
 yacht.orm = orm;
-validator.validate = validate;
-validator.__globals = __globals;
-orm.setUpdateFields = setUpdateFields;
-
